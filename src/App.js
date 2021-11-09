f19d7aea02da90caa0246491155b0a2c57a8c2e9
Raphael Vizoni do Prado <ravizoni@gmail.com>
2021-11-09 00:32:23 -0300

    Lazy Loading / Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import Header from './Components/Header'
-import Bag from './Pages/Bag'
-import Payment from './Pages/Payment'
-import Confirmation from './Pages/Confirmation'
 
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
+
+const BagPage = lazy(() => import('./Pages/Bag'))
+const PaymentPage = lazy(() => import('./Pages/Payment'))
+const ConfirmationPage = lazy(() => import('./Pages/Confirmation'))
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* fix header at the top */}
       <Header />
+
       <Switch>
+        <Suspense fallback={<div>Loading Page...</div>}>
+          <Route exact path="/bag" component={BagPage} />
+          <Route path="/payment" component={PaymentPage} />
+          <Route path="/confirmation" component={ConfirmationPage} />
+        </Suspense>
         <Route exact path="/" render={() => <Redirect to="bag" />} />
-        <Route exact path="/bag" component={Bag} />
-        <Route exact path="/payment" component={Payment} />
-        <Route exact path="/confirmation" component={Confirmation} />
       </Switch>
     </BrowserRouter>
   )
